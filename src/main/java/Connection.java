7258c5297b5827a0485ca9a05b46c75bcbb7f6a9
bjagodzinski <b.s.jagodzinski@gmail.com>
2016-09-08 09:08:54 +0200

    Update Connection.java

--- src/main/java/Connection.java
+++ src/main/java/Connection.java
@@ -5,6 +5,13 @@ import java.net.HttpURLConnection;
  * Created by Rafal on 2016-09-07.
  */
 public class Connection extends HttpURLConnection{
+    
+    // Why do you extend HttpURLConnection?
+    // Do you add some specific stuff? Do you want to handle something extra?
+    // If not, leave the idea about extending any class... :)
+    
+    // In general... 'PREFER COMPOSITION OVER INHERITENCE' !!!
+    
     public void disconnect() {
 
     }
